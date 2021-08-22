39200ec006731e17c8b380334b2d13e06eefe42a
sjpbailey <sjpbailey1961@gmail.com>
2021-08-22 03:07:01 -0700

    Commits

--- AmiNemController.py
+++ AmiNemController.py
@@ -1,49 +1,83 @@
+#!/usr/bin/env python3
+"""
+Polyglot v3 node server Example 1
+Copyright (C) 2021 Robert Paauwe
 
-
+MIT License
+"""
 import udi_interface
 import sys
 import time
-import urllib3
-import requests
 import xml.etree.ElementTree as ET
 
 LOGGER = udi_interface.LOGGER
-LOG_HANDLER = udi_interface.LOG_HANDLER
 Custom = udi_interface.Custom
-ISY = udi_interface.ISY 
+polyglot = None
+Parameters = None
+n_queue = []
+count = 0
 
-LOG_HANDLER.set_log_format('%(asctime)s %(threadName)-10s %(name)-18s %(levelname)-8s %(module)s:%(funcName)s: %(message)s')
-
+'''
+TestNode is the device class.  Our simple counter device
+holds two values, the count and the count multiplied by a user defined
+multiplier. These get updated at every shortPoll interval
+'''
 class AmiNemController(udi_interface.Node):
-    def __init__(self, polyglot, primary, address, name):
-        super(AmiNemController, self).__init__(polyglot, primary, address, name)
-        self.poly = polyglot
-        self.name = 'AMI NEM Controller'  # override what was passed in
-        self.hb = 0
-        self.Parameters = Custom(polyglot, 'customparams')
-        self.Notices = Custom(polyglot, 'notices')
-        self.poly.subscribe(self.poly.START, self.start, address)
-        self.poly.subscribe(self.poly.CUSTOMPARAMS, self.parameterHandler)
-        self.poly.subscribe(self.poly.POLL, self.poll)
-        self.poly.ready()
-        self.poly.addNode(self)
-        # Attributes
-        self.user = None
-        self.password = None
-        self.isy_ip = None
-        self.nem_oncor = None
-        self.isy = ISY(self.poly)
+    id = 'controller'
+    drivers = [
+            {'driver': 'ST', 'value': 1, 'uom': 2},
+            {'driver': 'GPV', 'value': 0, 'uom': 2},
+            {'driver': 'CC', 'value': 0, 'uom': 30},
+            {'driver': 'GV1', 'value': 0, 'uom': 73},
+            {'driver': 'TPW', 'value': 0, 'uom': 33},
+            {'driver': 'GV2', 'value': 0, 'uom': 33},
+            {'driver': 'GV3', 'value': 0, 'uom': 33},
+            ]
 
-    def parameterHandler(self, params):
-        self.Parameters.load(params)
-        LOGGER.debug('Loading parameters now')
-        self.check_params()    
+def noop(self):
+    LOGGER.info('Discover not implemented')
 
-    def start(self):
-        #self.poly.updateProfile()
-        self.discover()
+    commands = {'DISCOVER': noop}
 
-    def discover(self, *args, **kwargs):
+'''
+node_queue() and wait_for_node_event() create a simple way to wait
+for a node to be created.  The nodeAdd() API call is asynchronous and
+will return before the node is fully created. Using this, we can wait
+until it is fully created before we try to use it.
+'''
+def node_queue(data):
+    n_queue.append(data['address'])
+
+def wait_for_node_event():
+    while len(n_queue) == 0:
+        time.sleep(0.1)
+    n_queue.pop()
+
+'''
+Read the user entered custom parameters. In this case, it is just
+the 'multiplier' value.  Save the parameters in the global 'Parameters'
+'''
+def parameterHandler(params):
+    global Parameters
+
+    Parameters.load(params)
+
+
+'''
+This is where the real work happens.  When we get a shortPoll, increment the
+count, report the current count in GV0 and the current count multiplied by
+the user defined value in GV1. Then display a notice on the dashboard.
+'''
+def poll(polltype):
+    global count
+    global Parameters
+
+    if 'shortPoll' in polltype:
+        if Parameters['multiplier'] is not None:
+            mult = int(Parameters['multiplier'])
+        else:
+            mult = 1000
+
         amiem_resp = self.isy.cmd("/rest/emeter")
         amiem_count = 0
         amiem_count1 = 0
@@ -57,103 +91,75 @@ class AmiNemController(udi_interface.Node):
             #amiem_count = float(amiem_root('instantaneousDemand'))
             for amie in amiem_root.iter('instantaneousDemand'):
                 amiem_count = float(amie.text)
-                LOGGER.info("kW: " + str(amiem_count/float(self.nem_oncor)))
-                self.setDriver('CC', amiem_count/float(self.nem_oncor))
+                LOGGER.info("kW: " + str(amiem_count/float(mult)))
+                self.setDriver('CC', amiem_count/float(mult))
 
             #amiem_count1 = float(amiem_root.iter('instantaneousDemand'))
             for amie1 in amiem_root.iter('instantaneousDemand'):
                 amiem_count1 = float(amie1.text)
                 LOGGER.info("WATTS: " + str(amiem_count1))
-                self.setDriver('GV1', amiem_count1/float(self.nem_oncor)*1000)
+                self.setDriver('GV1', amiem_count1/float(mult)*1000)
 
             #ustdy_count = float(amiem_root.iter('currDayDelivered'))
             for ustd in amiem_root.iter('currDayDelivered'):
                 ustdy_count = float(ustd.text)
                 LOGGER.info("kWh: " + str(ustdy_count))
-                self.setDriver('TPW', ustdy_count/float(self.nem_oncor))
+                self.setDriver('TPW', ustdy_count/float(mult))
 
             #prevs_count = float(amiem_root.iter('previousDayDelivered'))
             for prev in amiem_root.iter('previousDayDelivered'):
                 prevs_count = float(prev.text)
                 LOGGER.info("kWh: " + str(prevs_count))
-                self.setDriver('GV2', prevs_count/float(self.nem_oncor))
+                self.setDriver('GV2', prevs_count/float(mult))
 
             #sumss_count = float(amiem_root.iter('currSumDelivered')#.text)
             for sums in amiem_root.iter('currSumDelivered'):
                 sumss_count = float(sums.text)
                 LOGGER.info("kWh: " + str(sumss_count))
-                self.setDriver('GV3', sumss_count/float(self.nem_oncor))
+                self.setDriver('GV3', sumss_count/float(mult))
 
-    def delete(self):
-        LOGGER.info('Deleting AMI NEM, Net Energy Meter')
 
-    def stop(self):
-        LOGGER.debug('AMI NEM NodeServer stopped.')
-
-    def set_module_logs(self,level):
-        LOGGER.getLogger('urllib3').setLevel(level)
-
-    def check_params(self):
-        self.Notices.clear()
-        default_nem_oncor = "1000"
-
-        self.nem_oncor = self.Parameters.nem_oncor
-        if self.nem_oncor is None:
-            self.nem_oncor = default_nem_oncor
-            LOGGER.error('check_params: Devisor for Oncor Meters not defined in customParams, please add it.  Using {}'.format(default_nem_oncor))
-            self.nem_oncor = default_nem_oncor 
-
-    def query(self, command=None):
-        nodes = self.poly.getNodes()
-        for node in nodes:
-            nodes[node].reportDrivers()
-
-    def poll(self, flag):
-        nodes = self.poly.getNodes()
-        for node in nodes:
-            nodes[node].reportDrivers()
-            self.discover()
-        if 'longPoll' in flag:
-            LOGGER.debug('longPoll (controller)')
-        else:
-            LOGGER.debug('shortPoll (controller)')
-            
-
-    def remove_notices_all(self,command):
-        LOGGER.info('remove_notices_all: notices={}'.format(self.Notices))
-        # Remove all existing notices
-        self.Notices.clear()
-
-    id = 'controller'
-    
-    commands = {
-        'QUERY': query,
-        'DISCOVER': discover,
-        'REMOVE_NOTICES_ALL': remove_notices_all,
-    }
-    drivers = [
-        {'driver': 'ST', 'value': 1, 'uom': 2},
-        {'driver': 'GPV', 'value': 0, 'uom': 2},
-        {'driver': 'CC', 'value': 0, 'uom': 30},
-        {'driver': 'GV1', 'value': 0, 'uom': 73},
-        {'driver': 'TPW', 'value': 0, 'uom': 33},
-        {'driver': 'GV2', 'value': 0, 'uom': 33},
-        {'driver': 'GV3', 'value': 0, 'uom': 33},
-    ]
+'''
+When we are told to stop, we update the node's status to False.  Since
+we don't have a 'controller', we have to do this ourselves.
+'''
+def stop():
+    nodes = polyglot.getNodes()
+    for n in nodes:
+        nodes[n].setDriver('ST', 0, True, True)
+    polyglot.stop()
 
 if __name__ == "__main__":
     try:
-        polyglot = udi_interface.Interface([AmiNemController])
+        polyglot = udi_interface.Interface([])
         polyglot.start()
-        control = AmiNemController(polyglot, 'controller', 'controller', 'AmiNemContoller')
+
+        Parameters = Custom(polyglot, 'customparams')
+
+        # subscribe to the events we want
+        polyglot.subscribe(polyglot.CUSTOMPARAMS, parameterHandler)
+        polyglot.subscribe(polyglot.ADDNODEDONE, node_queue)
+        polyglot.subscribe(polyglot.STOP, stop)
+        polyglot.subscribe(polyglot.POLL, poll)
+
+        # Start running
+        polyglot.ready()
+        polyglot.setCustomParamsDoc()
+        polyglot.updateProfile()
+
+        '''
+        Here we create the device node.  In a real node server we may
+        want to try and discover the device or devices and create nodes
+        based on what we find.  Here, we simply create our node and wait
+        for the add to complete.
+        '''
+        node = AmiNemController(polyglot, 'controller', 'controller', 'Utility Meter')
+        polyglot.addNode(node)
+        wait_for_node_event()
+
+        # Just sit and wait for events
         polyglot.runForever()
     except (KeyboardInterrupt, SystemExit):
-        LOGGER.warning("Received interrupt or exit...")
-        """
-        Catch SIGTERM or Control-C and exit cleanly.
-        """
-        polyglot.stop()
-    except Exception as err:
-        LOGGER.error('Excption: {0}'.format(err), exc_info=True)
-    sys.exit(0)
-    
+        sys.exit(0)
+        
+
